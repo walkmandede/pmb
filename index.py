898d6c9a6d4f266970e376409e362fbbd393e0d0
walkmandede <walkmandede@gmail.com>
2023-01-18 16:55:35 +0630

    second commit

--- index.py
+++ index.py
@@ -5,7 +5,13 @@ from routes.page_route import *
 from routes.task_route import *
 
 app = FastAPI()
-app.include_router(user)
-app.include_router(project)
-app.include_router(page)
-app.include_router(task)
+# app.include_router(user)
+# app.include_router(project)
+# app.include_router(page)
+# app.include_router(task)
+
+
+
+@app.get("/test")
+async def register():
+        return {"message" : "Deployed Successfully"}
